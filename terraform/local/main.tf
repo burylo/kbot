d271d53f6dd401c4acb439afe6fb9134c76a2f1f
Yurii Burylo <104938773+burylo@users.noreply.github.com>
2024-05-06 16:26:59 +0000

    Flux automation

---
+++ terraform/local/main.tf
@@ -0,0 +1,36 @@
+provider "kind" {
+  provider   = "docker"
+  kubeconfig = pathexpand("${path.module}/kind-config")
+}
+
+resource "kind_cluster" "this" {
+  name = "kind-cluster"
+  config = <<-EOF
+        apiVersion: kind.x-k8s.io/v1alpha4
+        kind: Cluster
+        nodes:
+        - role: control-plane
+        - role: worker
+    EOF
+}
+
+module "github_repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+}
+
+module "tls_private_key" {
+  source = "../modules/tf-hashicorp-tls-keys"
+}
+
+module "flux_bootstrap" {
+  source            = "../modules/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = "${path.module}/kind-config" # module.gke_cluster.kubeconfig
+  github_token      = var.GITHUB_TOKEN
+}
